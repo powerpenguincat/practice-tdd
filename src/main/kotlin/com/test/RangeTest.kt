d16afc50565602272089a7f23397549b371b83e5
ppc <ppc.develop@gmail.com>
2021-06-29 00:01:32 +0900

    Check a JUnit.

--- src/main/kotlin/com/test/RangeTest.kt
+++ src/main/kotlin/com/test/RangeTest.kt
@@ -1,4 +1,11 @@
 package com.test
 
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
 class RangeTest {
+    @Test
+    fun hoge(): Unit {
+        assertTrue(true)
+    }
 }
